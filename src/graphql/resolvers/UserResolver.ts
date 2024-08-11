e2603dfebcf974785d6e003ff4cb6e3bb5c4e542
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-11 18:52:02 +0200

    get all users

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -4,8 +4,13 @@ import { mockUsers } from 'src/__mocks__/mockUsers';
 
 @Resolver()
 export class userResolver {
-  @Query(() => User)
+  @Query(() => User, { nullable: true })
   getUserById(@Args('id', { type: () => Int }) id: number) {
     return mockUsers.find((user) => user.id === id);
+  }
+
+  @Query(() => [User], { nullable: true })
+  getAllUsers() {
+    return mockUsers;
   }
 }
