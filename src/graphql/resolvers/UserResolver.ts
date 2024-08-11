4bf3eb3810b686acb08ba873b0f0708ff01ffbcb
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-11 22:29:21 +0200

    mutation & resolveField

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,8 +1,18 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { User } from '../models/User';
 import { mockUsers } from 'src/__mocks__/mockUsers';
+import { UserSettings } from '../models/UsersSettings';
+import { mockUserSettings } from 'src/__mocks__/mockUserSettings';
 
-@Resolver()
+@Resolver(() => User)
 export class userResolver {
   @Query(() => User, { nullable: true })
   getUserById(@Args('id', { type: () => Int }) id: number) {
@@ -12,5 +22,26 @@ export class userResolver {
   @Query(() => [User], { nullable: true })
   getAllUsers() {
     return mockUsers;
+  }
+
+  @ResolveField(() => UserSettings, { name: 'settings', nullable: true })
+  getUserSettings(@Parent() user: User) {
+    return mockUserSettings.find((setting) => setting.userId === user.id);
+  }
+
+  @Mutation(() => User)
+  createUser(
+    @Args('username') username: string,
+    @Args('displayName', { nullable: true }) displayName: string,
+    @Args('email') email: string,
+  ) {
+    const newUser = {
+      id: Math.floor(Math.random() * 101),
+      username,
+      displayName,
+      email,
+    };
+    mockUsers.push(newUser);
+    return newUser;
   }
 }
